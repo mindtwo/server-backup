74db10430f11d3a381d889826d39703bbf0f4c3e
Jonas Emde <emde@mindtwo.de>
2020-05-14 16:56:27 +0200

    Add storage dir by default

--- config.example.php
+++ config.example.php
@@ -7,7 +7,7 @@ return [
         [
             'slug'        => 'production', // Used within filenames and as identifier in logs
             'source'      => '', // Relative or absolute path to the source directory
-            'destination' => '.storage/production', // Relative or absolute path to the backup directory
+            'destination' => 'storage/production', // Relative or absolute path to the backup directory
             'exclude'     => [  // Any allowed pattern for tar. Pathes relative the source directory is a good idea.
                 'releases/*',
                 'shared/repository',
@@ -17,7 +17,7 @@ return [
     'databases' => [
         [
             'slug'        => 'production', // Used within filenames and as identifier in logs
-            'destination' => '.storage/production/databases', // Relative or absolute path to the source directory
+            'destination' => 'storage/production/databases', // Relative or absolute path to the source directory
             'db_host'     => 'localhost',
             'db_user'     => '',
             'db_password' => '',
